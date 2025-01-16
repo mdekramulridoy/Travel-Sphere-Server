5cc108e0bf218e7b2086cf71f17265a921d45945
ekramulhoqueridoy <158032414+ekramulhoqueridoy@users.noreply.github.com>
2025-01-16 21:54:36 +0600

    fix a error

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { ObjectId } = require("mongodb");
 
-// Middleware 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// verify middleware
+const verifyToken = (req, res, next) => {
+  // console.log('inside verify token', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// verify admin
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const userCollection = client.db("travelDb").collection("users");
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if (!isAdmin) {
+    return res.status(403).send({ message: 'forbidden access' });
+  }
+  next();
+}
+
+
+
+
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tvoho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,43 +64,70 @@ async function run() {
     const packagesCollection = client.db("travelDb").collection("packages");
     const tourGuidesCollection = client.db("travelDb").collection("guides");
 
+    // jwt api working here
 
-
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr",
+      });
+      res.send({ token });
+    });
 
     // admin
 
-
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          role: 'admin'
-        }
-      }
-      const result = await userCollection.updateOne(filter, updatedDoc)
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
-    })
-    
+    });
+
     // user api
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken,verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
 
-    app.post('/users', async (req, res) => {
+
+    // admin check
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+
+
+
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
         return res.send({
-          message: 'user already exist', insertedId: null,
-        })
+          message: "user already exist",
+          insertedId: null,
+        });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
-    })
-
+    });
 
     // Endpoint to fetch all packages
     app.get("/packages", async (req, res) => {
@@ -106,8 +167,6 @@ async function run() {
           .json({ message: "Error fetching random packages", error });
       }
     });
-
-    
 
     // Endpoint to fetch all tour guides
     app.get("/guides", async (req, res) => {
